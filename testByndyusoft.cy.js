c1af1daadc7aa23662de313fa2281720be43650d
Stepan Abankin <stepan_mbr@mail.ru>
2023-08-28 19:03:12 -0300

    Test

--- testByndyusoft.cy.js
+++ testByndyusoft.cy.js
@@ -24,3 +24,28 @@ describe('verify contacts', () => {
     
 })
 
+describe('verify contacts', () => {
+    before (() => {
+        cy.visit('https://www.google.com/')
+        cy.get('#APjFqb').type('Byndyusoft')
+        cy.get('div.aajZCb > div.lJ9FBc > center > input.gNO89b').click()
+    })
+
+
+    it('should open new page', () => {
+        cy.get('a[href="https://byndyusoft.com/"]').invoke('removeAttr', 'target', '_self').contains('Byndyusoft').click()    
+    })
+
+    it('popup with contact information should appear', () => {
+        cy.get('body > section.knowMore > div > span').click()
+    })
+
+    it('popup should contain telephone number: 8 800 775-15-21', () => {
+        cy.get('div.popup-callback__content').should('contain', '8 800 775-15-21')       
+    })
+
+    it('popup should contain e-mail address: sales@byndyusoft.com', () => {
+        cy.get('div.popup-callback__content').should('contain', 'sales@byndyusoft.com')
+    })
+    
+})
